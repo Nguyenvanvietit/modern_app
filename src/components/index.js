529acccfac7a524a1b0cf55f287c9ef3de63b321
NguyenVanViet <it.nguyenvanviet@gmail.com>
2023-01-19 22:46:44 +0700

    commit

---
+++ src/components/index.js
@@ -0,0 +1,23 @@
+import Navbar from './Navbar';
+import Footer from './Footer';
+import Billing from './Billing';
+import CardDeal from './CardDeal';
+import Business from './Business';
+import Clients from './Clients';
+import CTA from './CTA'
+import Stats from './Stats';
+import Testimonials from './Testimonials';
+import Hero from './Hero';
+
+export {
+    Navbar,
+    Footer,
+    Billing,
+    CardDeal,
+    Business,
+    Clients,
+    CTA,
+    Stats,
+    Testimonials,
+    Hero
+};
